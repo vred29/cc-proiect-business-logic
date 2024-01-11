1ed474f9b20a2724668b727b48c86bab66d20bdc
isreactive <alexandru.mireuta@stud.acs.upb.ro>
2024-01-11 19:34:44 +0200

    Moving to maven

---
+++ src/main/java/com/amcr/appoiments/AppoimentsApplication.java
@@ -0,0 +1,13 @@
+package com.amcr.appoiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppoimentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amcr/appoiments/AppoimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amcr.appoiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppoimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
