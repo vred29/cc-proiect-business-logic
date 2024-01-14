85ee9599333d2abf0e025d2e0143fa37d507a81a
Cosmin <rosu_cosmin_2007@yahoo.com>
2024-01-14 15:52:05 +0200

    security config

---
+++ src/main/java/com/amcr/appointments/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.amcr.appointments.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> User
+                .withUsername("red") // Set your custom username
+                .password(passwordEncoder().encode("red")) // Set your custom password
+                .roles("USER")
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+}
